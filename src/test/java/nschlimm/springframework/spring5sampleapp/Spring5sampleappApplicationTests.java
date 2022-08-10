9776cc3f1f38d5156ab5f274719008c5aaf3d23e
Niklas Schlimm <ns103@hotmail.de>
2022-08-10 17:42:53 +0200

    Initial

---
+++ src/test/java/nschlimm/springframework/spring5sampleapp/Spring5sampleappApplicationTests.java
@@ -0,0 +1,13 @@
+package nschlimm.springframework.spring5sampleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5sampleappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
